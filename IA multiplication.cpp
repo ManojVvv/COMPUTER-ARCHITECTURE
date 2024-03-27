a019aaca5591cc26f0112951ac895c47d652e034
ManojVvv <112842928+ManojVvv@users.noreply.github.com>
2024-03-27 13:29:30 +0530

    Add files via upload

---
+++ IA multiplication.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int num1=9;
+		int num2=2;
+		int product;
+		product = num1 * num2;
+		printf("The product between %d and %d is %d\n",num1,num2,product);
+		return 0;
+}
